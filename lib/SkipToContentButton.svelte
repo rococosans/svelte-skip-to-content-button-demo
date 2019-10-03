62bfd15111a47380072a89f99354bf64bcefe1f2
luscan <dennyscakeslam@gmail.com>
2019-10-03 12:18:16 -0700

    initial

---
+++ lib/SkipToContentButton.svelte
@@ -0,0 +1,23 @@
+<a href="#content" class="skip-to-content-button">
+  Skip to Content
+</a>
+
+<style>
+  .skip-to-content-button {
+    position: absolute;
+    top: -100px;
+    left: 50%;
+    transform: translate3d(-50%, 0, 0);
+    pointer-events: none;
+    opacity: 0;
+    display: block;
+    transition: all 300ms ease;
+  }
+
+  .skip-to-content-button:focus {
+    transform: translate3d(-50%, 200px, 0);
+    pointer-events: all;
+    opacity: 1;
+  }
+
+</style>
---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import SkipToContentButton from '../lib/SkipToContentButton.svelte'
+	export let name;
+</script>
+
+<SkipToContentButton />
+
+<nav>
+	<ul>
+		<li><a href="#link1">link1</a></li>
+		<li><a href="#link2">link2</a></li>
+		<li><a href="#link3">link3</a></li>
+		<li><a href="#link4">link4</a></li>
+		<li><a href="#link5">link5</a></li>
+	</ul>
+</nav>
+
+<main id="content">
+	<h1>Hello {name}!</h1>
+	<a href="#hero-link">Learn more</a>
+</main>
+
+
+<style>
+	ul {
+		display: flex;
+	}
+
+	li {
+		list-style-type: none;
+	}
+
+	li+li {
+		margin-left: 24px;
+	}
+
+	h1 {
+		color: purple;
+	}
+
+</style>
